08df81e6b4a946d01cf08dfdb47009eacec2138b
LiJnD <luico199@gmal.com>
2022-11-23 11:49:32 -0500

    commit 3

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
-const productos = document.getElementById('producto');
+const productosFinal = document.querySelector('.productosSPaceFinal');
 
-document.addEventListener('DOMContentLoaded', e => {ProducFinalSpace()})
-
-const ProducFinalSpace = async () => {
-    const res = await fetch('https://finalspaceapi.com/api/v0/character')
-    const data = await res.json()
-    pintcards(data);
+function fechtSpace(id) {
+    fetch(`https://finalspaceapi.com/api/v0/${id}/`)
+        .then(res => res, json())
+        .then(data => console.log(data));
 }
 
-const pintcards = data =>{
-    console.log(data);
+function fechtSpaces(number) {
+    for (let i = 1; i <= number; i++) {
+        fechtSpace(i);
+    }
+}
+
+function creadorSpace(SpaceFinal) {
+    const card = document.createElement('div');
+    card.classList.add('space-block');
+
+    const speaceContainer = document.createElement('div');
+    speaceContainer.classList.add('img-container');
+    
+    const space = document.createElement('div');
+    space.src = SpaceFinal.img_url
+    
 }
